4831664395ff2891d10db786d480bf993e3f1871
Hei-7annik <7annik.hein@gmail.com>
2024-06-03 12:18:18 +0200

    FEAT: Add a optimized solution to Problem 1442
    abusing xor canceling out pairs of 1's a range of numbers resulting in zero using xor can be split at any point to gain two subranges that are equal under xor operation.
    
    Signed-off-by: Hei-7annik <7annik.hein@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,52 @@ impl Solution {
         }
         tuples.len() as u32
     }
+
+    pub fn count_triplets_optimized(array: &[u32]) -> u32 {
+        let mut count = 0;
+
+        // min window size is 2 because it needs to be split in two
+        for window_size in 2..=array.len() {
+            let mut xor_result = 0;
+
+            // setup for sliding window xor calculation
+            //
+            // Example: window_size = 3
+            // window       |< start i=3 |< end i=5
+            // array [1][2] | [3][4]     | [5][6][7][8]
+
+            array.iter()
+                .take(window_size - 1)
+                .for_each(|number| xor_result ^= number);
+
+
+            // max offset    #  #  #  #  #  |    window    |
+            // array        [1][2][3][4][5] | [6][7]...[n] |
+            let max_offset = array.len() - window_size;
+            for offset in 0..=max_offset {
+
+                // add new window[end] to this xor calculation
+                //
+                // window       |< start i=3 |< end i=6
+                // array [1][2] | [3][4][5]  | [6][7][8]
+                xor_result ^= array[offset + window_size - 1];
+                if xor_result == 0 {
+                    let start = offset;
+                    let end = offset + window_size - 1;
+                    (start + 1..end + 1).for_each(|mid| println!("({start},{mid},{end})"));
+
+                    count += (window_size - 1) as u32;
+                }
+
+                // remove old window[start] for next xor calculation
+                //
+                // window           |< start i=4 |< end i=6
+                // array [1][2][3]  | [4][5]     | [6][7][8]
+                xor_result ^= array[offset]
+            }
+        }
+        count
+    }
 }
 
 fn main() {
@@ -38,5 +84,5 @@ fn main() {
     assert!(vector.len().le(&MAX_VECTOR_SIZE));
     assert_eq!(vector.iter().find(|&x| { x > &MAX_INT_SIZE}),None);
 
-    println!("The number of triplets is: {}", Solution::count_triplets(&vector));
+    println!("The number of triplets is: {}", Solution::count_triplets_optimized(&vector));
 }
