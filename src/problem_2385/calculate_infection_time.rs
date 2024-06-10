94b88d8964e242fd4673f462f79f98e65a8aaf98
Hei-7annik <7annik.hein@gmail.com>
2024-06-10 22:18:42 +0200

    STYLE: Changed control flow to better showcase which paths get compared.
    Signed-off-by: Hei-7annik <7annik.hein@gmail.com>

--- src/problem_2385/calculate_infection_time.rs
+++ src/problem_2385/calculate_infection_time.rs
@@ -4,14 +4,14 @@ use std::cmp::{max};
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct TreeNode {
-    pub val: i32,
+    pub val: u32,
     pub left: Option<Rc<RefCell<TreeNode>>>,
     pub right: Option<Rc<RefCell<TreeNode>>>,
 }
 
 impl TreeNode {
     #[inline]
-    pub fn new(val: i32) -> Self {
+    pub fn new(val: u32) -> Self {
         TreeNode {
             val,
             left: None,
@@ -20,47 +20,55 @@ impl TreeNode {
     }
 }
 
-// Options
-// 1. The path from infected_node to furthest_leaf includes root
-// 2. The path root -> infected_node and root -> furthest_leaf is the same for the first n steps
-// 3. The path root -> infected_node is part of root -> furthest_leaf
 
-pub fn calculate_infection_time_naive(root: Option<Rc<RefCell<TreeNode>>>, start: i32) -> i32 {
+
+pub fn calculate_infection_time_naive(root: Option<Rc<RefCell<TreeNode>>>, start: u32) -> u32 {
     depth_mapping(root, start, 0).longest_path
 }
 
 #[derive(Debug)]
 pub struct Results {
-    longest_path: i32,
-    infected: Option<i32>,
+    longest_path: u32,
+    infected: Option<u32>,
 }
 
-pub fn depth_mapping(root: Option<Rc<RefCell<TreeNode>>>, target: i32, current_depth: i32) -> Results {
-    let mut result = Results {longest_path: current_depth - 1, infected: None };
+/* Options
+1.  Infection starts on one and travels to the other side of the tree
+    longest path:   infected node -> ancestor -> ... -> root -> ... -> child
 
-    // walk through
+2.  Infection starts on one and travels to the other side, which gets fully infected before completing
+    the side it started on.
+    longest path:   infected node -> ancestor -> ... -> ancestor -> .. -> child
+
+3.  Infection takes longer travels to one of the subtrees of its node then through the rest of the tree
+    longest path:   infected node -> child -> ... -> child
+*/
+pub fn depth_mapping(root: Option<Rc<RefCell<TreeNode>>>, target: u32, current_depth: u32) -> Results {
     if let Some(node) = root {
-        let result_left = depth_mapping(node.borrow().left.clone(), target, current_depth + 1);
-        let result_right = depth_mapping(node.borrow().right.clone(), target, current_depth + 1);
+        let left_subtree = depth_mapping(node.borrow().left.clone(), target, current_depth + 1);
+        let right_subtree = depth_mapping(node.borrow().right.clone(), target, current_depth + 1);
 
-        result.longest_path = max(result_left.longest_path, result_right.longest_path);
-
-        // Option 1 & 2
-        if let Some(infected) = result_left.infected {
-            let path_infected_to_right_leaf = result_left.infected.unwrap() - 2* current_depth + result_right.longest_path;
-            result.longest_path = max(path_infected_to_right_leaf, result_left.longest_path);
-            result.infected = Some(infected);
-        }
-        else if let Some(infected) = result_right.infected {
-            let path_infected_to_left_leaf = result_right.infected.unwrap() - 2* current_depth + result_left.longest_path;
-            result.longest_path = max(path_infected_to_left_leaf, result_right.longest_path);
-            result.infected = Some(infected);
-        }
-
-        // Option 3
+        // Set baseline for Option 3
         if node.borrow().val == target {
-            result.infected = Some(current_depth);
+            let infected = Some(current_depth);
+            let longest_path = max(left_subtree.longest_path - current_depth, right_subtree.longest_path - current_depth);
+
+            return Results{longest_path, infected}
         }
+        else {
+            let infected = left_subtree.infected.or(right_subtree.infected);
+            // Determine which side of the tree is infected
+            let longest_path = match infected {
+                // Option 1 & 2
+                Some(infected_node_depth) if left_subtree.infected.is_some() =>
+                    max(infected_node_depth - 2* current_depth + right_subtree.longest_path, left_subtree.longest_path),
+                Some(infected_node_depth) if right_subtree.infected.is_some() =>
+                    max(infected_node_depth - 2* current_depth + left_subtree.longest_path, right_subtree.longest_path),
+                // Option 3
+                _ => max(left_subtree.longest_path, right_subtree.longest_path)
+            };
+            return Results{longest_path, infected}
+        };
     }
-    result
+    Results {longest_path: current_depth - 1, infected: None }
 }
