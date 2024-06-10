63c2dfa210a9db7f573f825c3c7ac8ae9d071a88
Hei-7annik <7annik.hein@gmail.com>
2024-06-10 21:56:29 +0200

    TEST: Add testcases for problem 2385 including multiple different trees and path lengths
    Signed-off-by: Hei-7annik <7annik.hein@gmail.com>

--- Tests/problem_2385_solution_verification.rs
+++ Tests/problem_2385_solution_verification.rs
@@ -1,11 +1,54 @@
 #[cfg(test)]
 mod test {
-    use leetcode::problem_2385::{};
+    use std::rc::Rc;
+    use std::cell::RefCell;
+    use leetcode::problem_2385::calculate_infection_time::{calculate_infection_time_naive, TreeNode};
+
+    fn assemble_tree(values: &[u32], position: usize) -> Option<Rc<RefCell<TreeNode>>> {
+        if let Some(&val) = values.get(position) {
+            if val == 0 {
+                return None
+            }
+            return Some(Rc::new(RefCell::new(TreeNode {
+                val,
+                left: assemble_tree(values, 2*position + 1),
+                right: assemble_tree(values, 2*position + 2)}
+            )))
+        }
+        None
+    }
 
     mod with_naive_solution {
         use super::*;
+        #[test]
+        fn where_longest_path_includes_root() {
+            let root = assemble_tree(&[1,5,3,0,4,10,6,0,0,9,2], 0);
+            assert_eq!(calculate_infection_time_naive(root, 3), 4)
+        }
+
+        #[test]
+        fn where_longest_path_doesnt_include_root() {
+            let root = assemble_tree(&[1,5,3,7,4,10,6,0,0,9,2,0,0,0,0,0,0,0,0,11], 0);
+            assert_eq!(calculate_infection_time_naive(root, 7), 4)
+        }
+
+        #[test]
+        fn where_longest_path_is_from_infected_node_to_a_leaf() {
+            let root = assemble_tree(&[1,5,3,0,4,10,6,0,0,9,2], 0);
+            assert_eq!(calculate_infection_time_naive(root, 1), 3)
+        }
+
+        #[test]
+        fn where_longest_path_is_zero() {
+            let root = assemble_tree(&[1], 0);
+            assert_eq!(calculate_infection_time_naive(root, 1), 0)
+        }
     }
     mod with_optimized_solution {
         use super::*;
+        /*#[test]
+        fn in_random_input() {
+            let root = assemble_tree(&[1,5,3,0,4,10,6,0,0,9,2], 0);
+        }*/
     }
 }
