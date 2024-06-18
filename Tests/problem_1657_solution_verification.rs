2583ac6a3d968de8b1125be21236f557793a991f
Hei-7annik <7annik.hein@gmail.com>
2024-06-18 17:43:13 +0200

    FEAT: Add given boilerplate code for problem 1657
    Signed-off-by: Hei-7annik <7annik.hein@gmail.com>

---
+++ Tests/problem_1657_solution_verification.rs
@@ -0,0 +1,18 @@
+#[cfg(test)]
+mod test {
+    use leetcode::problem_1657::close_strings::{close_strings};
+
+    mod with_naive_solution {
+        use super::*;
+
+        #[test]
+        fn where_() {}
+    }
+
+    mod with_optimized_solution {
+        use super::*;
+
+        #[test]
+        fn where_() {}
+    }
+}
