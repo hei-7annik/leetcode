ae1455e32acbd3035004cf00f00439936474dd51
Hei-7annik <7annik.hein@gmail.com>
2024-06-19 16:25:54 +0200

    TEST: Add 3 basic test cases for problem 1657
    Signed-off-by: Hei-7annik <7annik.hein@gmail.com>

--- Tests/problem_1657_solution_verification.rs
+++ Tests/problem_1657_solution_verification.rs
@@ -6,13 +6,22 @@ mod test {
         use super::*;
 
         #[test]
-        fn where_() {}
+        fn with_word1_abc_and_word2_bca() {
+            assert!(close_strings(String::from("abc"), String::from("bca")))
+        }
+
+        #[test]
+        fn with_word1_a_and_word2_aa() {
+            assert!(close_strings(String::from("a"), String::from("a")))
+        }
+
+        #[test]
+        fn with_word1_cabbba_and_word2_abbccc() {
+            assert!(close_strings(String::from("cabbba"), String::from("abbccc")))
+        }
     }
 
     mod with_optimized_solution {
-        use super::*;
-
-        #[test]
-        fn where_() {}
+        // use super::*;
     }
 }
