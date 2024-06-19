5e6d3f15468881627da726fa6996fcf0a0660608
Hei-7annik <7annik.hein@gmail.com>
2024-06-19 17:41:36 +0200

    PERF: Reduce copying features for each comparison
    Signed-off-by: Hei-7annik <7annik.hein@gmail.com>

--- src/problem_1657/close_strings.rs
+++ src/problem_1657/close_strings.rs
@@ -19,23 +19,22 @@ fn get_char_set_and_char_distribution(word: String) -> BTreeMap<char, u32> {
 // Operation 2 allows for changing how often characters occur
 // if and only if there is another (kind of) character to swap
 
-
 pub fn close_strings(word1: String, word2: String) -> bool {
     if word1.len() != word2.len() {
         false
     }
     else {
         let features_word1 = get_char_set_and_char_distribution(word1);
-        let char_set_w1: Vec<char> = features_word1.keys().cloned().collect();
-        let mut char_distribution_w1: Vec<u32> = features_word1.values().cloned().collect();
-        char_distribution_w1.sort();
-
         let features_word2 = get_char_set_and_char_distribution(word2);
-        let char_set_w2: Vec<char> = features_word2.keys().cloned().collect();
-        let mut char_distribution_w2: Vec<u32> = features_word2.values().cloned().collect();
-        char_distribution_w2.sort();
 
-        char_set_w1 == char_set_w2 &&
-            char_distribution_w1 == char_distribution_w2
+        let chars_subset_equal = features_word1.keys().any(|key| features_word2.contains_key(key));
+        let mut chars_distribution_w1: Vec<u32> = features_word1.into_values().collect();
+        chars_distribution_w1.sort();
+
+        let mut chars_distribution_w2: Vec<u32> = features_word2.into_values().collect();
+        chars_distribution_w2.sort();
+
+        chars_subset_equal &&
+            chars_distribution_w1 == chars_distribution_w2
     }
 }
