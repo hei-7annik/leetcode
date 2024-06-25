dff9d981f432c48a293f3badca5580b8441b29ce
Hei-7annik <7annik.hein@gmail.com>
2024-06-25 19:53:22 +0200

    TEST: Add 2 given testcases contained in leetcode problem 368
    Signed-off-by: Hei-7annik <7annik.hein@gmail.com>

--- Tests/problem_368_solution_verification.rs
+++ Tests/problem_368_solution_verification.rs
@@ -1,17 +1,24 @@
 #[cfg(test)]
 mod test {
-    use leetcode::problem_368::largest_divisible_subset::{};
+    use leetcode::problem_368::largest_divisible_subset::largest_divisible_subset;
 
     mod with_naive_solution {
         use super::*;
 
         #[test]
-        fn where_() {
-
+        fn where_the_numbers_steadily_increase() {
+            let numbers = Vec::from([1,2,3]);
+            assert_eq!(largest_divisible_subset(numbers), [1,2]);
+        }
+        #[test]
+        fn where_the_numbers_are_powers_of_two() {
+            let numbers = Vec::from([1,2,4,8]);
+            assert_eq!(largest_divisible_subset(numbers), [1,2,4,8]);
         }
     }
 
     mod with_optimized_solution {
         // use super::*;
+
     }
 }
