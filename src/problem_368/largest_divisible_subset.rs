ca9d83eba6fc9a43e412c30224af5ddc7ecd1e25
Hei-7annik <7annik.hein@gmail.com>
2024-06-27 10:40:10 +0200

    FIX: Add backtracking after each decision, to add a number to the subset, ensure the largest one is found
    Signed-off-by: Hei-7annik <7annik.hein@gmail.com>

--- src/problem_368/largest_divisible_subset.rs
+++ src/problem_368/largest_divisible_subset.rs
@@ -1,23 +1,18 @@
 // multiply all numbers in the solution + 1 additional a factor
 
-pub fn largest_divisible_subset(mut nums: Vec<i32>) -> Vec<i32> {
-    nums.sort();
-    nums.reverse();
+pub fn largest_divisible_subset(numbers: &[u32]) -> Vec<u32> {
+    let mut subset = Vec::new();
 
-    let mut answer = Vec::new();
+    for index in 1..numbers.len() {
+        let num = numbers[index];
+        if num % numbers[0] == 0 {
 
-    while let Some(new_factor) = nums.pop() {
-        // ToDo: fix impact of ordering in input numbers -> least limiting choice
-        if answer.iter().all(|factor| new_factor % factor == 0) {
-            answer.push(new_factor);
+            let addition_factors = largest_divisible_subset(&numbers[index..]);
+            if addition_factors.len() > subset.len() {
+                subset = addition_factors;
+            }
         }
-
-        nums = nums.iter().filter_map(|&number| { if number % new_factor == 0 {
-            Some(number)
-        }
-        else {
-            None
-        }}).collect();
     }
-    return answer
+    subset.insert(0, numbers[0]);
+    return subset
 }
