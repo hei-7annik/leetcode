666df4f3e4b0b9df4a880e4489b6b09632317113
Hei-7annik <7annik.hein@gmail.com>
2024-06-28 22:49:26 +0200

    REFACT: Algorithm now makes proper use of dynamic programming
    Signed-off-by: Hei-7annik <7annik.hein@gmail.com>

--- src/problem_368/largest_divisible_subset.rs
+++ src/problem_368/largest_divisible_subset.rs
@@ -1,3 +1,5 @@
+use std::collections::BTreeMap;
+
 // each factor f starts a subset S:
 // each number N divisible by f_i creates a variant of that subset
 // containing N as factor f_(i+1) where both subsets are equal for
@@ -27,33 +29,40 @@ pub fn divisible_subset(numbers: &[u32], current_factor: u32) -> Vec<u32> {
     return current_subset
 }
 
-pub fn largest_divisible_subset_optimized(mut numbers: Vec<u32>) -> Vec<u32> {
-    numbers.sort();
-
-    let result = divisible_subset_optimized(&numbers[..], 1);
-    return result[1..].to_vec()
+pub fn largest_divisible_subset_optimized(numbers: Vec<u32>) -> Vec<u32> {
+    let mut auto_complete = BTreeMap::new();
+    return divisible_subset_optimized(&numbers, 1, &mut auto_complete);
 }
 
-fn divisible_subset_optimized(numbers: &[u32], current_factor: u32) -> Vec<u32> {
-    let mut current_subset = Vec::new();
-    for i in 0..numbers.len() {
-        let alternative;
+fn divisible_subset_optimized(numbers: &Vec<u32>, current_factor: u32, auto_complete: &mut BTreeMap<u32, Vec<u32>>) -> Vec<u32> {
+    let filtered_numbers: Vec<u32> = numbers.iter().filter_map(|&n| match n % current_factor {
+        0 => Some(n),
+        _ => None
+    }).collect();
 
-        if numbers[i] % current_factor == 0 {
-            // BRANCH MITIGATION
-            if let Some(index) = current_subset.iter().position(|&n| n == numbers[i]) {
-                alternative = Vec::from(&current_subset[index..])
+    let mut result = Vec::new();
+    let mut current_longest_subset = Vec::new();
+
+    for &factor in filtered_numbers.iter() {
+        if factor == current_factor {
+            result.push(current_factor);
+        }
+        else {
+            let subset;
+            if let Some(s) = auto_complete.get(&current_factor) {
+                subset = s.clone();
             }
-            // BRANCHING
             else {
-
-                alternative = divisible_subset_optimized(&numbers[i+1..], numbers[i]);
+                subset = divisible_subset_optimized(&filtered_numbers, factor, auto_complete);
             }
-            if alternative.len() > current_subset.len() {
-                current_subset = alternative;
+
+            if subset.len() > current_longest_subset.len() {
+                current_longest_subset = subset;
             }
         }
     }
-    current_subset.insert(0, current_factor);
-    return current_subset
+    auto_complete.insert(current_factor, current_longest_subset.clone());
+    result.append(&mut current_longest_subset);
+
+    return result
 }
