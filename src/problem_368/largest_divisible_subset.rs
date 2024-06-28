48e03d66ccfa7ef150ab75fcd950076171889448
Hei-7annik <7annik.hein@gmail.com>
2024-06-28 14:44:50 +0200

    FEAT: Add a optimized solution using dynamic programming
    Signed-off-by: Hei-7annik <7annik.hein@gmail.com>

--- src/problem_368/largest_divisible_subset.rs
+++ src/problem_368/largest_divisible_subset.rs
@@ -4,7 +4,6 @@
 // the first i elements
 //
 // the largest subset is obtained by BRANCHING for each N
-
 pub fn largest_divisible_subset(numbers: &[u32]) -> Vec<u32> {
     let mut current_subset = Vec::new();
     let current_factor = numbers[0];
@@ -14,6 +13,33 @@ pub fn largest_divisible_subset(numbers: &[u32]) -> Vec<u32> {
         if numbers[i] % current_factor == 0 {
             // BRANCHING
             let alternative = largest_divisible_subset(&numbers[i..]);
+            if alternative.len() > current_subset.len() {
+                current_subset = alternative;
+            }
+        }
+    }
+    current_subset.insert(0, current_factor);
+    return current_subset
+}
+
+pub fn largest_divisible_subset_optimized(numbers: &[u32]) -> Vec<u32> {
+    let mut current_subset = Vec::new();
+    let current_factor = numbers[0];
+
+    for i in 1..numbers.len() {
+
+        let alternative;
+
+        let num = numbers[i];
+        if num % current_factor == 0 {
+            // BRANCH MITIGATION
+            if let Some(index) = current_subset.iter().position(|&n| n == num) {
+                alternative = Vec::from(&current_subset[index..])
+            }
+            // BRANCHING
+            else {
+                alternative = largest_divisible_subset(&numbers[i..]);
+            }
             if alternative.len() > current_subset.len() {
                 current_subset = alternative;
             }
