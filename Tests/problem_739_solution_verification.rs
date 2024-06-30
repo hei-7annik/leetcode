6b9d1bccfff290a2ecbc84e5c25c12c2e99a73e5
Hei-7annik <7annik.hein@gmail.com>
2024-06-30 11:24:44 +0200

    FEAT: Add give boilerplate code for problem 739
    Signed-off-by: Hei-7annik <7annik.hein@gmail.com>

---
+++ Tests/problem_739_solution_verification.rs
@@ -0,0 +1,17 @@
+#[cfg(test)]
+mod test {
+    use leetcode::problem_739::{};
+    mod unoptimized_solution {
+        use super::*;
+
+        #[test]
+        fn with_() {
+
+        }
+    }
+
+    mod partially_optimized_solution {
+        use super::*;
+
+    }
+}
