8307e37c1c1ec673f4730490de9abbbdf84868f3
Hei-7annik <7annik.hein@gmail.com>
2024-06-30 11:24:44 +0200

    FEAT: Add given boilerplate code for problem 739
    Signed-off-by: Hei-7annik <7annik.hein@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -13,3 +13,7 @@ pub mod problem_1657 {
 pub mod problem_368 {
     pub mod largest_divisible_subset;
 }
+
+pub mod problem_739 {
+    pub mod daily_temperatures;
+}
---
+++ src/problem_739/daily_temperatures.rs
@@ -0,0 +1,3 @@
+pub fn daily_temperatures(temperatures: Vec<u32>) -> Vec<u32> {
+
+}
