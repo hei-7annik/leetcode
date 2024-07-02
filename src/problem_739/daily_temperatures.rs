56c77d92ac998f58d24654b498e2ea492f8693b3
Hei-7annik <7annik.hein@gmail.com>
2024-07-02 20:58:39 +0200

    FEAT: Add nested loop based solution for problem 739
    Signed-off-by: Hei-7annik <7annik.hein@gmail.com>

--- src/problem_739/daily_temperatures.rs
+++ src/problem_739/daily_temperatures.rs
@@ -1,3 +1,18 @@
 pub fn daily_temperatures(temperatures: Vec<u32>) -> Vec<u32> {
+    let mut results= vec![0u32;temperatures.len()];
+    for day in 0..temperatures.len() {
+        let temp = temperatures[day];
+        
+        for future_day in (day + 1)..temperatures.len() {
+            if temperatures[future_day] > temp {
+                results[day] = (future_day - day) as u32;
+                break;
+            }
+        }
+    }
+    results
+}
 
+pub fn daily_temperatures_optimized(temperatures: Vec<u32>) -> Vec<u32> {
+    temperatures
 }
