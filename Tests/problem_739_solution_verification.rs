a53c02bc5ec6d301b1e3166bd4539ec496fd9de6
Hei-7annik <7annik.hein@gmail.com>
2024-07-02 20:06:35 +0200

    TEST: Add 3 basic testcases contained in leetcode problem 739 description
    Signed-off-by: Hei-7annik <7annik.hein@gmail.com>

--- Tests/problem_739_solution_verification.rs
+++ Tests/problem_739_solution_verification.rs
@@ -1,12 +1,25 @@
 #[cfg(test)]
 mod test {
-    use leetcode::problem_739::{};
+    use leetcode::problem_739::daily_temperatures::{daily_temperatures};
     mod unoptimized_solution {
         use super::*;
 
         #[test]
         fn with_() {
+            let temps = vec![73,74,75,71,69,72,76,73];
+            assert_eq!(daily_temperatures(temps), vec![1,1,4,2,1,1,0,0])
+        }
 
+        #[test]
+        fn with_() {
+            let temps = vec![30,40,50,60];
+            assert_eq!(daily_temperatures(temps), vec![1,1,1,0])
+        }
+
+        #[test]
+        fn with_() {
+            let temps = vec![30,60,90];
+            assert_eq!(daily_temperatures(temps), vec![1,1,0])
         }
     }
 
