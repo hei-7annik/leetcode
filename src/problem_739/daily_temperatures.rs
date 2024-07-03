cd95cce33159b7a52373ea7d6a30a82ca47632ef
Hei-7annik <7annik.hein@gmail.com>
2024-07-03 10:48:38 +0200

    FEAT: Add a stack based optimized solution
    Signed-off-by: Hei-7annik <7annik.hein@gmail.com>

--- src/problem_739/daily_temperatures.rs
+++ src/problem_739/daily_temperatures.rs
@@ -2,7 +2,7 @@ pub fn daily_temperatures(temperatures: Vec<u32>) -> Vec<u32> {
     let mut results= vec![0u32;temperatures.len()];
     for day in 0..temperatures.len() {
         let temp = temperatures[day];
-        
+
         for future_day in (day + 1)..temperatures.len() {
             if temperatures[future_day] > temp {
                 results[day] = (future_day - day) as u32;
@@ -13,6 +13,21 @@ pub fn daily_temperatures(temperatures: Vec<u32>) -> Vec<u32> {
     results
 }
 
-pub fn daily_temperatures_optimized(temperatures: Vec<u32>) -> Vec<u32> {
+pub fn daily_temperatures_optimized(mut temperatures: Vec<u32>) -> Vec<u32> {
+    let mut stack = Vec::new();
+
+    for day in 0..temperatures.len() {
+        while let Some(&some_day_before) = stack.last() {
+            if temperatures[day] > temperatures[some_day_before] {
+                temperatures[some_day_before] = (day - some_day_before) as u32;
+                stack.pop();
+            }
+            else {
+                break;
+            }
+        }
+        stack.push(day);
+    }
+    stack.iter().for_each(|&i| temperatures[i] = 0);
     temperatures
 }
