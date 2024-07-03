cd5f44b0d1178d48519afea42f34112e2a8516a0
Hei-7annik <7annik.hein@gmail.com>
2024-07-03 10:49:21 +0200

    TEST: Added tests for optimized solution, changed naming to describe test cases
    Signed-off-by: Hei-7annik <7annik.hein@gmail.com>

--- Tests/problem_739_solution_verification.rs
+++ Tests/problem_739_solution_verification.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod test {
-    use leetcode::problem_739::daily_temperatures::{daily_temperatures};
+    use leetcode::problem_739::daily_temperatures::{daily_temperatures, daily_temperatures_optimized};
     mod unoptimized_solution {
         use super::*;
 
@@ -11,20 +11,38 @@ mod test {
         }
 
         #[test]
-        fn with_() {
+        fn with_increasing_temps_short() {
+            let temps = vec![30,60,90];
+            assert_eq!(daily_temperatures(temps), vec![1,1,0])
+        }
+
+        #[test]
+        fn with_increasing_temps_long() {
             let temps = vec![30,40,50,60];
             assert_eq!(daily_temperatures(temps), vec![1,1,1,0])
         }
 
-        #[test]
-        fn with_() {
-            let temps = vec![30,60,90];
-            assert_eq!(daily_temperatures(temps), vec![1,1,0])
-        }
     }
 
     mod partially_optimized_solution {
         use super::*;
 
+        #[test]
+        fn with_() {
+            let temps = vec![73,74,75,71,69,72,76,73];
+            assert_eq!(daily_temperatures_optimized(temps), vec![1,1,4,2,1,1,0,0])
+        }
+
+        #[test]
+        fn with_increasing_temps_short() {
+            let temps = vec![30,60,90];
+            assert_eq!(daily_temperatures_optimized(temps), vec![1,1,0])
+        }
+
+        #[test]
+        fn with_increasing_temps_long() {
+            let temps = vec![30,40,50,60];
+            assert_eq!(daily_temperatures_optimized(temps), vec![1,1,1,0])
+        }
     }
 }
