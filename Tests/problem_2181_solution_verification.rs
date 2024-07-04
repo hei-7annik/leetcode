6e177ca99480ab2ff67d90cec560d6b3468991ed
Hei-7annik <7annik.hein@gmail.com>
2024-07-04 19:30:38 +0200

    FEAT: Add given boilerplate code for problem 2181
    Signed-off-by: Hei-7annik <7annik.hein@gmail.com>

---
+++ Tests/problem_2181_solution_verification.rs
@@ -0,0 +1,14 @@
+#[cfg(test)]
+mod test {
+    use std::boxed::Box;
+    use leetcode::problem_2181::merge_nodes{merge_nodes};
+
+
+    mod naive_solution {
+        use super::*;
+        #[test]
+        fn where_() {
+
+        }
+    }
+}
---
+++ src/problem_2181/merge_nodes.rs
@@ -0,0 +1,21 @@
+use std::boxed::Box;
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+  pub val: i32,
+  pub next: Option<Box<ListNode>>
+}
+
+impl ListNode {
+  #[inline]
+  fn new(val: i32) -> Self {
+    ListNode {
+      next: None,
+      val
+    }
+  }
+}
+
+pub fn merge_nodes(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    Some(Box::new(ListNode::new(0)))
+}
