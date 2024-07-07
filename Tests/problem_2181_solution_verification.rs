aa11d5fae21d5669b7ca24291334f1b5c1e42b83
Hei-7annik <7annik.hein@gmail.com>
2024-07-07 11:33:03 +0200

    TEST: Add tests for optimized solution to problem 2181
    Signed-off-by: Hei-7annik <7annik.hein@gmail.com>

--- Tests/problem_2181_solution_verification.rs
+++ Tests/problem_2181_solution_verification.rs
@@ -1,5 +1,5 @@
 mod test {
-    use leetcode::problem_2181::merge_nodes::{merge_nodes, list_from};
+    use leetcode::problem_2181::merge_nodes::{merge_nodes_naive, merge_nodes_optimized, list_from};
 
     mod naive_solution {
         use super::*;
@@ -7,14 +7,31 @@ mod test {
         fn with_list_containing_two_sections() {
             let list = list_from(&[0,3,1,0,4,5,2,0]);
             let result = list_from(&[4,11]);
-            assert_eq!(merge_nodes(list), result)
+            assert_eq!(merge_nodes_naive(list), result)
         }
 
         #[test]
         fn with_list_containing_three_sections() {
             let list = list_from(&[0,1,0,3,0,2,2,0]);
             let result = list_from(&[1,3,4]);
-            assert_eq!(merge_nodes(list), result)
+            assert_eq!(merge_nodes_naive(list), result)
+        }
+    }
+
+    mod optimized_solution {
+        use super::*;
+        #[test]
+        fn with_list_containing_two_sections() {
+            let list = list_from(&[0,3,1,0,4,5,2,0]);
+            let result = list_from(&[4,11]);
+            assert_eq!(merge_nodes_optimized(list), result)
+        }
+
+        #[test]
+        fn with_list_containing_three_sections() {
+            let list = list_from(&[0,1,0,3,0,2,2,0]);
+            let result = list_from(&[1,3,4]);
+            assert_eq!(merge_nodes_optimized(list), result)
         }
     }
 }
