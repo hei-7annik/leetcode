a87572bf919b690f23c73be20e35d1882506501a
Hei-7annik <7annik.hein@gmail.com>
2024-07-09 13:40:46 +0200

    FEAT: Add index based and partition/reordering based solution
    Signed-off-by: Hei-7annik <7annik.hein@gmail.com>

--- src/problem_1823/find_the_winner.rs
+++ src/problem_1823/find_the_winner.rs
@@ -1,3 +1,42 @@
-pub fn find_the_winner(n: i32, k: i32) -> i32 {
+use std::collections::VecDeque;
 
+pub fn find_the_winner(n: i32, k: i32) -> i32 {
+    if k == 1 {
+        return n
+    }
+
+    let mut friends: Vec<i32> = (1..=n).collect();
+    let mut friends_in_the_game = n;
+
+    let mut index = -1;
+    while friends_in_the_game > 1 {
+        let mut steps = k;
+
+        while steps > 0 {
+            index = (index + 1) % n;
+            if friends[index as usize] != 0 {
+                steps -= 1;
+            }
+        }
+
+        friends[index as usize] = 0;
+        friends_in_the_game -= 1;
+    }
+
+    *friends.iter().find(|&&friend| friend != 0).unwrap()
+}
+
+pub fn find_the_winner_optimized(n: i32, k: i32) -> i32 {
+    let mut friends: VecDeque<i32> = (1..=n).collect();
+
+    while friends.len() > 1 {
+        for _i in 0..k - 1 {
+            let friend = friends.pop_front().unwrap();
+            friends.push_back(friend);
+        }
+
+        friends.pop_front();
+    }
+
+    friends.pop_front().unwrap()
 }
