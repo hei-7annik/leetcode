b5e271f710789ebc04403320392f9555f3e15f86
Hei-7annik <7annik.hein@gmail.com>
2024-07-09 13:39:45 +0200

    TEST: Add edge case for index based solution
    Signed-off-by: Hei-7annik <7annik.hein@gmail.com>

--- Tests/problem_1823_solution_verification.rs
+++ Tests/problem_1823_solution_verification.rs
@@ -4,20 +4,25 @@ mod problem_1823 {
     mod naive_solution {
         use super::*;
         #[test]
-        fn with_5_friends_and_3_as_integer() {
-            assert_eq!(find_the_winner(5,2), 3)
+        fn with_5_friends_and_2_as_integer() {
+            assert_eq!(find_the_winner(5,3), 3)
         }
 
         #[test]
         fn with_6_friends_and_5_as_integer() {
             assert_eq!(find_the_winner(6,5), 1)
         }
+
+        #[test]
+        fn with_3_friends_and_1_as_integer() {
+            assert_eq!(find_the_winner(3,1), 3)
+        }
     }
 
     mod optimized_solution {
         use super::*;
         #[test]
-        fn with_5_friends_and_3_as_integer() {
+        fn with_5_friends_and_2_as_integer() {
             assert_eq!(find_the_winner_optimized(5,2), 3)
         }
 
