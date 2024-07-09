9d0cc548e359093d456619a6b3a11f7733b174f5
Hei-7annik <7annik.hein@gmail.com>
2024-07-09 13:23:34 +0200

    Revert "FEAT: TEST: Add 2 basic testcases contained in leetcode problem 1823 description for both optimized and unoptimized solutions"
    This reverts commit 2eef178599833886159d7d6404eca706e2782f5b.

--- Tests/problem_1823_solution_verification.rs
+++ Tests/problem_1823_solution_verification.rs
@@ -1,29 +1,20 @@
-mod problem_1823 {
-    use leetcode::problem_1823::find_the_winner::{find_the_winner, find_the_winner_optimized};
+mod test {
+    use leetcode::problem_1823::find_the_winner::{find_the_winner};
 
     mod naive_solution {
         use super::*;
         #[test]
-        fn with_5_friends_and_3_as_integer() {
-            assert_eq!(find_the_winner(5,2), 3)
+        fn with_() {
+            assert_eq!(find_the_winner(), )
         }
 
         #[test]
-        fn with_6_friends_and_5_as_integer() {
-            assert_eq!(find_the_winner(6,5), 1)
+        fn with_() {
+            assert_eq!(find_the_winner(), )
         }
     }
 
     mod optimized_solution {
         use super::*;
-        #[test]
-        fn with_5_friends_and_3_as_integer() {
-            assert_eq!(find_the_winner_optimized(5,2), 3)
-        }
-
-        #[test]
-        fn with_6_friends_and_5_as_integer() {
-            assert_eq!(find_the_winner_optimized(6,5), 1)
-        }
     }
 }
