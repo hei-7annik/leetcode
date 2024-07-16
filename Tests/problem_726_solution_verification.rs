34614b6926dcd9fefc88e8f79afcf662c1b76058
Hei-7annik <7annik.hein@gmail.com>
2024-07-16 16:41:04 +0200

    TEST: Add 4 basic testcases contained in leetcode problem 726 description
    Signed-off-by: Hei-7annik <7annik.hein@gmail.com>

--- Tests/problem_726_solution_verification.rs
+++ Tests/problem_726_solution_verification.rs
@@ -5,19 +5,36 @@ mod problem_726 {
         use super::*;
 
         #[test]
-        fn with_() {
-            let formula = "".to_string();
-            assert_eq!(count_of_atoms(formula), "")
+        fn with_H2O() {
+            let formula = "H2O".to_string();
+            assert_eq!(count_of_atoms(formula), "H2O")
         }
+        #[test]
+        fn with_one_parenthesis() {
+            let formula = "Mg(OH)2".to_string();
+            assert_eq!(count_of_atoms(formula), "H2MgO2")
+        }
+        #[test]
+        fn with_nested_parenthesis() {
+            let formula = "K4(ON(SO3)2)2".to_string();
+            assert_eq!(count_of_atoms(formula), "K4N2O14S4")
+        }
+
+        #[test]
+        fn with_a_lot_of_atoms() {
+            let formula = "Be32".to_string();
+            assert_eq!(count_of_atoms(formula), "Be32")
+        }
+
     }
 
-    mod partially_optimized_solution {
+    mod optimized_solution {
         use super::*;
 
         #[test]
         fn with_() {
-            let formula = "".to_string();
-            assert_eq!(count_of_atoms(formula), "")
+            let formula = "H2O".to_string();
+            //assert_eq!(count_of_atoms(formula), "H2O")
         }
     }
 }
