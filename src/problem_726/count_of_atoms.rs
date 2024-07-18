1e395d55fc459b57b7be6c73ee5690bca9b71964
Hei-7annik <7annik.hein@gmail.com>
2024-07-18 21:55:49 +0200

    REFACT: optimized memory usage and simplified/unified match arms and result formula construction
    Signed-off-by: Hei-7annik <7annik.hein@gmail.com>

--- src/problem_726/count_of_atoms.rs
+++ src/problem_726/count_of_atoms.rs
@@ -1,45 +1,54 @@
 use std::collections::BTreeMap;
+///
+/// To-Do Add documentation to each function
+///
 pub fn count_of_atoms(formula: String) -> String {
-    let mut multiplier = Vec::from([1]);
-    let mut occurence = String::new();
+    let mut factors = vec![1];
+    let mut factor_len = 0;
+    let mut symbol_len = 0;
 
-    let mut element_distribution: BTreeMap<String, u32> = BTreeMap::new();
-    let mut periodic_table_element_symbol = String::with_capacity(2);
+    let mut element_count: BTreeMap<&str, u32> = BTreeMap::new();
 
-    formula.chars().rev().for_each(|char|
-        match char {
+    formula.chars().rev().enumerate().for_each(|(index, character)| {
+        let i = formula.len() - 1 - index;
+        match character {
             C if C.is_ascii_uppercase() => {
-                periodic_table_element_symbol.insert(0, C);
-                if let Some(old_occurrences) = element_distribution.insert(
-                    periodic_table_element_symbol.clone(),
-                    occurence.parse::<u32>().unwrap_or(1) * multiplier.last().unwrap()) {
-                        element_distribution
-                            .entry(periodic_table_element_symbol.clone())
-                            .and_modify(|occurences| *occurences += old_occurrences);
-                };
-                
-                periodic_table_element_symbol.clear();
-                occurence.clear()},
-            c if c.is_ascii_lowercase() => {
-                periodic_table_element_symbol.push(c)},
-            number if number.is_digit(10) =>
-                occurence.insert(0, number),
+                let periodic_table_element_symbol = &formula[i..=i + symbol_len];
+                let count_string = &formula[i + symbol_len + 1..=i + symbol_len + factor_len];
+                let count = count_string.parse().unwrap_or(1) * factors.last().unwrap();
+
+                element_count
+                    .entry(periodic_table_element_symbol)
+                    .and_modify(|old_count| *old_count += count)
+                    .or_insert(count);
+
+                factor_len = 0;
+                symbol_len = 0;
+            },
+            c if c.is_ascii_lowercase() =>
+                symbol_len += 1,
+            number if number.is_digit(10) => {
+                factor_len += 1;
+            },
             '(' => {
-                multiplier.pop(); ()},
+                factors.pop(); ()
+            },
             ')' => {
-                multiplier.push(occurence.parse::<u32>().unwrap_or(1) * multiplier.last().unwrap());
-                occurence.clear()},
+                let factor = &formula[i + 1..=i + factor_len];
+                factors.push(factor.parse().unwrap_or(1) * factors.last().unwrap());
+                factor_len = 0;
+            },
             _ => (),
         }
-    );
-    let mut result = String::new();
-    element_distribution.iter()
-        .for_each(|(key, value)| {
-            result.push_str(&key);
-            if *value > 1 {
-                result.push_str(&value.to_string());
-            }
-        });
+    });
 
-    result
+    element_count
+        .iter()
+        .fold(String::with_capacity(element_count.len() * 3),|mut result, (&symbol, &count)| {
+            result.push_str(symbol);
+            if count != 1 {
+                result.push_str(&count.to_string())
+            };
+            result
+        })
 }
