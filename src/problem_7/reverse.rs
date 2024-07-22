1e85506c10be8165335cd9b522851c700d47e8fe
Hei-7annik <7annik.hein@gmail.com>
2024-07-22 21:04:24 +0200

    FEAT: Add modulo based digit by digit construction algorithm for building the reverse number
    Signed-off-by: Hei-7annik <7annik.hein@gmail.com>

--- src/problem_7/reverse.rs
+++ src/problem_7/reverse.rs
@@ -1,6 +1,21 @@
+/// Construct reversed_number from number - digit by digit
 ///
-/// to-do add description
+/// Edge-Cases: min/max i32 values, number that, reversed are lager/smaller then max/min i32...
 ///
 pub fn reverse(x: i32) -> i32 {
+    let mut number = x;
 
+    let mut reversed_number = 0;
+
+    while number >= 10 || number.is_negative() && number <= -10 {
+        reversed_number += number % 10;
+        number /= 10;
+
+        match reversed_number.checked_mul(10) {
+            Some(res) => reversed_number = res,
+            None => return 0,
+        }
+    }
+
+    reversed_number.checked_add(number).unwrap_or_default()
 }
