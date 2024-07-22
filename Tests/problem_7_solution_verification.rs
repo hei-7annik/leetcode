b3e17c2a992ab602f8cd871709fd51545cbaf087
Hei-7annik <7annik.hein@gmail.com>
2024-07-22 21:02:47 +0200

    TEST: add tests for edgecases regarding limits of i32
    Signed-off-by: Hei-7annik <7annik.hein@gmail.com>

--- Tests/problem_7_solution_verification.rs
+++ Tests/problem_7_solution_verification.rs
@@ -9,14 +9,40 @@ mod problem_7 {
             assert_eq!(reverse(number), 321);
         }
 
+        #[test]
         fn with_negative_number() {
             let number = -123;
             assert_eq!(reverse(number), -321);
         }
 
+        #[test]
         fn with_last_digit_is_zero() {
             let number = 120;
             assert_eq!(reverse(number), 21);
+        }
+
+        #[test]
+        fn with_leading_zeros() {
+            let number = 00120;
+            assert_eq!(reverse(number), 21);
+        }
+
+        #[test]
+        fn with_only_one_digit() {
+            let number = 0;
+            assert_eq!(reverse(number), 0);
+        }
+
+        #[test]
+        fn with_max_i32_number() {
+            let number = 2147483647;
+            assert_eq!(reverse(number), 0);
+        }
+
+        #[test]
+        fn with_min_i32_number() {
+            let number = -2147483648;
+            assert_eq!(reverse(number), 0);
         }
     }
 }
