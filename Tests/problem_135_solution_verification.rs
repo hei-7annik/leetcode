49a85b85c0f3ec6868237d3ebee4eb9ea674e520
Hei-7annik <7annik.hein@gmail.com>
2024-07-31 22:16:25 +0200

    TEST: Add multiple cases for different configurations of rating triplets
    (a < b < c)
    (a > b > c)
    (a > b < c)
    ...
    
    Signed-off-by: Hei-7annik <7annik.hein@gmail.com>

--- Tests/problem_135_solution_verification.rs
+++ Tests/problem_135_solution_verification.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod problem_135 {
-    use leetcode::problem_135::candy::{candy};
+    use leetcode::problem_135::candy::{candy, candy_optimized};
     mod solution {
         use super::*;
         #[test]
@@ -14,5 +14,78 @@ mod problem_135 {
             let ratings = vec![1,2,2];
             assert_eq!(candy(ratings),4);
         }
+
+        #[test]
+        fn with_one_rating() {
+            let ratings = vec![1];
+            assert_eq!(candy(ratings),1);
+        }
+
+        #[test]
+        fn with_increasing_decreasing_and_equal_ratings() {
+            let ratings = vec![1,2,3,6,4,2,3,4,0,0];
+            assert_eq!(candy(ratings),20);
+        }
+
+        #[test]
+        fn with_increasing_and_decreasing_ratings() {
+            let ratings = vec![1,2,3,6,4,2,3,4];
+            assert_eq!(candy(ratings),18);
+        }
+
+        #[test]
+        fn with_equal_increasing_and_decreasing_ratings_with_peak() {
+            let ratings = vec![1,2,3,6,4,3,2,1];
+            assert_eq!(candy(ratings),21);
+        }
+
+        #[test]
+        fn with_equal_increasing_and_decreasing_ratings_without_peak() {
+            let ratings = vec![2,3,6,6,3,2];
+            assert_eq!(candy(ratings),12);
+        }
+
+        #[test]
+        fn with_more_decreasing_than_increasing_ratings_with_peak() {
+            let ratings = vec![2,3,8,6,3,2,1];
+            assert_eq!(candy(ratings),18);
+        }
+
+        #[test]
+        fn with_increasing_decreasing_ratings() {
+            let ratings = vec![1,0,2,3,4];
+            assert_eq!(candy(ratings),12);
+        }
+
+        #[test]
+        fn with_decreasing_ratings() {
+            let ratings = vec![4,3,2,1,0];
+            assert_eq!(candy(ratings),15);
+        }
+
+        #[test]
+        fn with_increasing_ratings() {
+            let ratings = vec![1,2,3,4,5];
+            assert_eq!(candy(ratings),15);
+        }
+
+        #[test]
+        fn with_equal_ratings() {
+            let ratings = vec![4,4,4,4,4];
+            assert_eq!(candy(ratings),5);
+        }
+
+        #[test]
+        fn with_decreasing_and_equal_ratings() {
+            let ratings = vec![4,3,2,2,2,1,0];
+            assert_eq!(candy(ratings),13);
+        }
+
+        #[test]
+        fn with_two_ratings() {
+            let ratings = vec![3,4];
+            assert_eq!(candy(ratings),3);
+        }
+
     }
 }
