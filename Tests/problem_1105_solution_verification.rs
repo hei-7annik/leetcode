0e1262670e987adc30797f64d1171c1b7776a699
Hei-7annik <7annik.hein@gmail.com>
2024-08-02 11:32:31 +0200

    TEST: Add 2 given testcases contained in leetcode problem 1105
    Signed-off-by: Hei-7annik <7annik.hein@gmail.com>

---
+++ Tests/problem_1105_solution_verification.rs
@@ -0,0 +1,18 @@
+#[cfg(test)]
+mod problem_1105 {
+    use leetcode::problem_1105::min_height_shelves::{min_height_shelves};
+    mod solution {
+        use super::*;
+        #[test]
+        fn with_seven_books_and_two_thicknesses() {
+            let books = [[1,1],[2,3],[2,3],[1,1],[1,1],[1,1],[1,2]].iter().collect();
+            assert_eq!(min_height_shelves(books, 4), 6);
+        }
+
+        #[test]
+        fn with_three_books_and_three_thicknesses() {
+            let books = [[1,3],[2,4],[3,2]].iter().collect();
+            assert_eq!(min_height_shelves(books, 6), 4);
+        }
+    }
+}
