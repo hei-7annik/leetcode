7d1ab84199835db3b347d7362540b7f52c0c9663
Hei-7annik <7annik.hein@gmail.com>
2024-08-13 21:49:05 +0200

    TEST: Fix type mismatch of input and tested function
    Signed-off-by: Hei-7annik <7annik.hein@gmail.com>

--- Tests/problem_1105_solution_verification.rs
+++ Tests/problem_1105_solution_verification.rs
@@ -5,13 +5,19 @@ mod problem_1105 {
         use super::*;
         #[test]
         fn with_seven_books_and_two_thicknesses() {
-            let books = [[1,1],[2,3],[2,3],[1,1],[1,1],[1,1],[1,2]].iter().collect();
+            let books = [[1,1],[2,3],[2,3],[1,1],[1,1],[1,1],[1,2]]
+                .iter()
+                .map(|book| book.to_vec())
+                .collect();
             assert_eq!(min_height_shelves(books, 4), 6);
         }
 
         #[test]
         fn with_three_books_and_three_thicknesses() {
-            let books = [[1,3],[2,4],[3,2]].iter().collect();
+            let books = [[1,3],[2,4],[3,2]]
+                .iter()
+                .map(|book| book.to_vec())
+                .collect();
             assert_eq!(min_height_shelves(books, 6), 4);
         }
     }
