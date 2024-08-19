4eff1e9f102757b88860c57d9993072315dfca3b
Hei-7annik <7annik.hein@gmail.com>
2024-08-19 22:23:50 +0200

    TEST: Add two additional testcases
    Signed-off-by: Hei-7annik <7annik.hein@gmail.com>

--- Tests/problem_1105_solution_verification.rs
+++ Tests/problem_1105_solution_verification.rs
@@ -20,5 +20,23 @@ mod problem_1105 {
                 .collect();
             assert_eq!(min_height_shelves(books, 6), 4);
         }
+
+        #[test]
+        fn with_two_narrow_tall_and_two_wide_and_short_books() {
+            let books = [[7,3],[8,7],[2,7],[2,5]]
+                .iter()
+                .map(|book| book.to_vec())
+                .collect();
+            assert_eq!(min_height_shelves(books, 10), 15);
+        }
+
+        #[test]
+        fn with_a_square_book() {
+            let books = [[9,9],[5,4],[3,1],[1,5],[7,3]]
+                .iter()
+                .map(|book| book.to_vec())
+                .collect();
+            assert_eq!(min_height_shelves(books, 10), 17);
+        }
     }
 }
