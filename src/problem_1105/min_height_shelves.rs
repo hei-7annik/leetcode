cd2f601a8f64569fe3fc19175d15ec9b0a8006e1
Hei-7annik <7annik.hein@gmail.com>
2024-08-20 22:50:06 +0200

    DOC: Extend documentation to include an explanation. Redo Case and Method section.
    Signed-off-by: Hei-7annik <7annik.hein@gmail.com>

--- src/problem_1105/min_height_shelves.rs
+++ src/problem_1105/min_height_shelves.rs
@@ -1,4 +1,5 @@
 /// Explanation
+/// Put books on a bookshelf with minimal height through optimal configuration of books on each shelf
 ///
 /// # Example
 /// ```rust
@@ -11,40 +12,40 @@
 /// ```
 ///
 /// # Cases
-/// 1. Does a book fit on the shelves
-/// 2. Is a book higher than all other books on the shelves
+/// 1. Shelf height does not increase with the new book
+/// 2. Shelf height increase is minimal with the new book on a shelf with previous books
+/// 3. Shelf height increase is minimal with the new book on a new shelf
 ///
 /// # Method
-/// 1. Split right before accumulated width exceeds `shelf_width`
-/// 2. Identify the highest book in each shelf
-/// 3. Optimize by moving high books onto previous/next shelve
-///
-/// Lower bound roundUp(sum(books.width) / shelf.width)
-/// Upper bound sum(books)
+/// 1. Treat each book like it's the last one.
+/// 2. For each book: Find the selection of previous books resulting in minimal `shelf_height`.
+/// 3. Add `shelf_height` to the `total_shelf_height` calculated for the bookcase, back when the
+///    first book not on the shelf was the last book.
 ///
 
 pub fn min_height_shelves(books: Vec<Vec<i32>>, shelf_width: i32) -> i32 {
-    let mut combined_shelf_height = Vec::from([0]);
+    let mut total_shelf_height = Vec::from([0]);
 
     for i in 0..books.len() {
         let mut shelf_height = 0;
         let mut book_stack_width = 0;
 
-        let new_height = combined_shelf_height
-            .iter()
+        let height = total_shelf_height.iter()
             .zip(&books[..=i])
             .rev()
             .take_while(|(_, &ref book)| {
+                // going back, select as many books as will fit on one shelf
                 book_stack_width += book[0];
                 book_stack_width <= shelf_width
             })
             .map( |(bookcase_height, book)| {
                 shelf_height = i32::max(shelf_height, book[1]);
+
                 bookcase_height + shelf_height
             }).min();
 
-        combined_shelf_height.push(new_height.unwrap());
+        total_shelf_height.push(height.unwrap());
     }
 
-    combined_shelf_height.pop().unwrap()
+    total_shelf_height.pop().unwrap()
 }
