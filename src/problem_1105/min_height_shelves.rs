0ca62734130eb3191bb6e6fba489df18a7e4b42e
Hei-7annik <7annik.hein@gmail.com>
2024-08-20 17:13:05 +0200

    FEAT: Add a dynamic programming based solution to problem 1105
    Signed-off-by: Hei-7annik <7annik.hein@gmail.com>

--- src/problem_1105/min_height_shelves.rs
+++ src/problem_1105/min_height_shelves.rs
@@ -11,38 +11,40 @@
 /// ```
 ///
 /// # Cases
-/// 1. Does a book fit in the shelf
-/// 2. Is a book higher than all other books on the shelf
+/// 1. Does a book fit on the shelves
+/// 2. Is a book higher than all other books on the shelves
 ///
 /// # Method
 /// 1. Split right before accumulated width exceeds `shelf_width`
 /// 2. Identify the highest book in each shelf
-/// 3. Optimize by moving high books onto previous/next shelf
+/// 3. Optimize by moving high books onto previous/next shelve
 ///
 /// Lower bound roundUp(sum(books.width) / shelf.width)
 /// Upper bound sum(books)
 ///
+
 pub fn min_height_shelves(books: Vec<Vec<i32>>, shelf_width: i32) -> i32 {
-    let mut current_shelf_capacity= shelf_width as u32;
-    let mut current_shelf_height: i32 = 0;
-    let mut combined_shelf_height: i32 = 0;
+    let mut combined_shelf_height = Vec::from([0]);
 
+    for i in 0..books.len() {
+        let mut shelf_height = 0;
+        let mut book_stack_width = 0;
 
-    books.iter().for_each(|book| {
-        let thickness = book[0] as u32;
-        let height = book[1];
+        let new_height = combined_shelf_height
+            .iter()
+            .zip(&books[..=i])
+            .rev()
+            .take_while(|(_, &ref book)| {
+                book_stack_width += book[0];
+                book_stack_width <= shelf_width
+            })
+            .map( |(bookcase_height, book)| {
+                shelf_height = i32::max(shelf_height, book[1]);
+                bookcase_height + shelf_height
+            }).min();
 
-        if let Some(reduced_capacity) = current_shelf_capacity.checked_sub(thickness) {
-            current_shelf_capacity = reduced_capacity;
-        }
-        else {
-            combined_shelf_height += current_shelf_height;
+        combined_shelf_height.push(new_height.unwrap());
+    }
 
-            current_shelf_capacity = shelf_width as u32 - thickness;
-            current_shelf_height = 0;
-        }
-        current_shelf_height = i32::max(height, current_shelf_height);
-    });
-
-    combined_shelf_height + current_shelf_height
+    combined_shelf_height.pop().unwrap()
 }
