0217d120d1890e3d07a312a2825746d2fc9b9eb6
Hei-7annik <7annik.hein@gmail.com>
2024-08-25 17:05:43 +0200

    FEAT: limit memory usage by only caching the last n intermediate results
    Signed-off-by: Hei-7annik <7annik.hein@gmail.com>

--- src/problem_1105/min_height_shelves.rs
+++ src/problem_1105/min_height_shelves.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 /// Explanation
 /// Put books on a bookshelf with minimal height through choosing optimal selection of which books go on
 /// each shelf
@@ -24,29 +26,28 @@
 ///    first book not on the shelf was the last book.
 ///
 
-pub fn min_height_shelves(books: Vec<Vec<i32>>, shelf_width: i32) -> i32 {
-    let mut bookshelf_heights = Vec::from([0]);
+pub fn min_height_shelves(books: Vec<Vec<i32>>, shelf_width: usize) -> i32 {
+    let mut bookshelf_heights = VecDeque::with_capacity(shelf_width + 1);
+    bookshelf_heights.push_front(0);
 
     for i in 0..books.len() {
         let mut additional_shelf_height = 0;
-        let mut sum_book_widths = 0;
+        let mut width = 0;
 
         let height = bookshelf_heights.iter()
-            .zip(&books[..=i])
-            .rev()
+            .zip(books[..=i].iter().rev())
             .take_while(|(_, &ref book)| {
                 // going back, select as many books as will fit on one shelf
-                sum_book_widths += book[0];
-                sum_book_widths <= shelf_width
+                width += book[0];
+                width <= shelf_width as i32
             })
-            .map( |(bookcase_height, book)| {
+            .map( |(bookshelf_height, book)| {
                 additional_shelf_height = i32::max(additional_shelf_height, book[1]);
-
-                bookcase_height + additional_shelf_height
+                bookshelf_height + additional_shelf_height
             }).min();
 
-        bookshelf_heights.push(height.unwrap());
+        bookshelf_heights.push_front(height.unwrap());
+        bookshelf_heights.truncate(shelf_width);
     }
-
-    bookshelf_heights.pop().unwrap()
+    *bookshelf_heights.front().unwrap()
 }
